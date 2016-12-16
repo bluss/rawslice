0bdf37377f1c8c3cc1e18f2757cd940902786dfd
bluss <bluss@users.noreply.github.com>
2016-12-17 00:06:48 +0100

    MAINT: Factor out data offsetting in tests

--- tests/quick.rs
+++ tests/quick.rs
@@ -7,40 +7,35 @@ extern crate rawslice;
 
 use rawslice::SliceIter;
 
+const MAX_OFFSET: usize = 15;
+
+// use offset for a random alignment of the data
+fn offset<T>(v: &[T], offset: usize) -> &[T] {
+    if v.len() == 0 {
+        return v;
+    }
+    let offset = (offset % MAX_OFFSET) % v.len();
+    &v[offset..]
+}
 
 // SliceIter
 quickcheck! {
-    fn slice_iter_find(v: Vec<i8>, offset: u8, pat: i8) -> bool {
-        // use offset for a random alignment of the data
-        if v.len() == 0 {
-            return true;
-        }
-        let offset = offset as usize % v.len();
-        let data = &v[offset..];
+    fn slice_iter_find(v: Vec<i8>, off: usize, pat: i8) -> bool {
+        let data = offset(&v, off);
 
         data.iter().find(|x| **x == pat) ==
             SliceIter::from(data).find(|x| **x == pat)
     }
 
-    fn slice_iter_position(v: Vec<i8>, offset: u8, pat: i8) -> bool {
-        // use offset for a random alignment of the data
-        if v.len() == 0 {
-            return true;
-        }
-        let offset = offset as usize % v.len();
-        let data = &v[offset..];
+    fn slice_iter_position(v: Vec<i8>, off: usize, pat: i8) -> bool {
+        let data = offset(&v, off);
 
         data.iter().position(|x| *x == pat) ==
             SliceIter::from(data).position(|x| *x == pat)
     }
 
-    fn slice_iter_rposition(v: Vec<i8>, offset: u8, pat: i8) -> bool {
-        // use offset for a random alignment of the data
-        if v.len() == 0 {
-            return true;
-        }
-        let offset = offset as usize % v.len();
-        let data = &v[offset..];
+    fn slice_iter_rposition(v: Vec<i8>, off: usize, pat: i8) -> bool {
+        let data = offset(&v, off);
 
         data.iter().rposition(|x| *x == pat) ==
             SliceIter::from(data).rposition(|x| *x == pat)
