0ee60ca2fc4404023d91857617c1edb99dc20d30
bluss <bluss@users.noreply.github.com>
2019-09-27 22:24:42 +0200

    Add more benchmarks

--- benches/unroll.rs
+++ benches/unroll.rs
@@ -9,11 +9,46 @@ use rawslice::SliceIter;
 
 const LEN: usize = 1024 * 1024;
 
+// Wrap an iterator and provide no smarts - just the .next() method
+struct WrapIter<I>(I);
+
+impl<I> Iterator for WrapIter<I> where I: Iterator {
+    type Item = I::Item;
+    fn next(&mut self) -> Option<Self::Item> { self.0.next() }
+}
+
 fn all0_default(b: &mut Bencher) {
     let mut v = vec![0u8; LEN];
     v[LEN - 1] = black_box(1);
     b.iter(|| {
         SliceIter::from(&v[..]).all(|&x| x == 0)
+    });
+    b.bytes = v.len() as u64;
+}
+
+fn all0_default_wrap(b: &mut Bencher) {
+    let mut v = vec![0u8; LEN];
+    v[LEN - 1] = black_box(1);
+    b.iter(|| {
+        WrapIter(SliceIter::from(&v[..])).all(|&x| x == 0)
+    });
+    b.bytes = v.len() as u64;
+}
+
+fn all0_std_iter(b: &mut Bencher) {
+    let mut v = vec![0u8; LEN];
+    v[LEN - 1] = black_box(1);
+    b.iter(|| {
+        v[..].iter().all(|&x| x == 0)
+    });
+    b.bytes = v.len() as u64;
+}
+
+fn all0_std_iter_wrap(b: &mut Bencher) {
+    let mut v = vec![0u8; LEN];
+    v[LEN - 1] = black_box(1);
+    b.iter(|| {
+        WrapIter(v[..].iter()).all(|&x| x == 0)
     });
     b.bytes = v.len() as u64;
 }
@@ -27,9 +62,23 @@ fn position_default(b: &mut Bencher) {
     b.bytes = v.len() as u64;
 }
 
+fn position_std_iter(b: &mut Bencher) {
+    let mut v = vec![0u8; LEN];
+    v[LEN - 1] = black_box(1);
+    b.iter(|| {
+        v[..].iter().position(|&x| x != 0)
+    });
+    b.bytes = v.len() as u64;
+}
+
+
 benchmark_group!(benches,
                  all0_default,
-                 position_default
+                 all0_default_wrap,
+                 all0_std_iter,
+                 all0_std_iter_wrap,
+                 position_default,
+                 position_std_iter
 );
 
 benchmark_main!(benches);
